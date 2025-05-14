7afb38296da4f6a03c1abe764b39a791ca3d3a74
Héctor Daniel Micheletti <hectorcba@gmail.com>
2025-05-14 09:30:04 -0300

    completo

---
+++ js/credenciales.js
@@ -0,0 +1,4 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '760155015699-8qig4nmj9p5nj8thhecsvda1662pdtd6.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyDohAVFty85MYdfLjCHuVrkROj4zMSJp3U';
+const SPREADSHEET = "1j6D1-TPr5_H7JsGYi24Aa33RdLO2U00MCvCKpJih1lo";
