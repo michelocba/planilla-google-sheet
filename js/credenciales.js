7de0cd3672356d35dc9b5210d8a07055dff77c2b
Héctor Daniel Micheletti <hectorcba@gmail.com>
2025-05-23 13:53:27 -0300

    se cambio el sheet

--- js/credenciales.js
+++ js/credenciales.js
@@ -1,4 +1,5 @@
 // TODO(developer): Set to client ID and API key from the Developer Console
 const CLIENT_ID = '760155015699-8qig4nmj9p5nj8thhecsvda1662pdtd6.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyDohAVFty85MYdfLjCHuVrkROj4zMSJp3U';
-const SPREADSHEET = "1j6D1-TPr5_H7JsGYi24Aa33RdLO2U00MCvCKpJih1lo";
+// const SPREADSHEET = "1j6D1-TPr5_H7JsGYi24Aa33RdLO2U00MCvCKpJih1lo";
+const SPREADSHEET = "1h9b2GuU_OTVyL9RUBCHTw_zl73Nii91PktpVnmj3OiI";
